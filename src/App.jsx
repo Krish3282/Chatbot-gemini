aa4bd9ec7611559dd06a97a91723dddde7fe2b98
Krish3282 <kvkrishna1938@gmail.com>
2025-06-16 14:12:58 +0530

    added file to git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SideBar from './component/Sidebar/SideBar'
+import Main from './component/main/Main'
+
+const App = () => {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  )
+}
+
+export default App
